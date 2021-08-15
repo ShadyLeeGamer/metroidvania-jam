103aed566ff4cb7dff88fae1cfabb3dde45f692f
willowed <willowed.jade@gmail.com>
2021-08-15 15:29:08 -0400

    fixed horizontal movement not drifting long enough

--- Metroidvania Jam/Assets/Scripts/PlayerMovement.cs
+++ Metroidvania Jam/Assets/Scripts/PlayerMovement.cs
@@ -17,25 +17,36 @@ public class PlayerMovement : MonoBehaviour
 	public float accelerateTime = 0.5f;
 	public float decelerateTime = 0.5f;
 	float hTimer = 0;
+	float oldDeltaVX = 0;
+	float initialVX = 0;
 	void MoveHorizontal(float hInput) {
 		// Calculate the correct speed based on inputs
-		float desiredVelocityX = horizontalSpeed * hInput;
-		float deltaVelocityX = desiredVelocityX - rb.velocity.x;
-		if (deltaVelocityX == 0) return;
+		float desiredVX = horizontalSpeed * hInput;
+		float deltaVX = desiredVX - rb.velocity.x;
+		// Reset timer when new acceleration / deceleration begins
+		if (oldDeltaVX == 0) hTimer = 0;
+		if ((deltaVX > 0 && oldDeltaVX < 0) ||
+			(deltaVX < 0 && oldDeltaVX > 0)) {
+			hTimer = 0;
+		}
+		oldDeltaVX = deltaVX;
+		if (deltaVX == 0 || (deltaVX < 0.0001f && deltaVX > -0.0001f))
+			return;
 
 		// Linearly adjust player velocity when accelerating or decelerating
-		float newVelocityX;
-		float maxTime = (deltaVelocityX > 0)?
+		float newVX;
+		float maxTime = (deltaVX * hInput > 0)?
 			accelerateTime : decelerateTime;
-		if (hTimer < maxTime) { // adjust velocity
+		if (hTimer < maxTime) { // linearly adjust velocity
+			float slope = deltaVX / (maxTime - hTimer);
 			hTimer += Time.deltaTime;
-			newVelocityX = rb.velocity.x + deltaVelocityX * hTimer / maxTime;
+			newVX = rb.velocity.x + slope * Time.deltaTime;
 		}
-		else { // acceleration or deceleration has finished
+		else { // Reset timer when acceleration / deceleration has finished
 			hTimer = 0;
-			newVelocityX = desiredVelocityX;
+			newVX = desiredVX;
 		}
-		rb.velocity = new Vector2(newVelocityX, rb.velocity.y);
+		rb.velocity = new Vector2(newVX, rb.velocity.y);
 	}
 
 	void Update() {
